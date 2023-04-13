b49f8491eba033ede9624b3501892d529332ae83
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-13 11:32:38 +0600

    03. dataTypes

---
+++ 03. dataTypes/main.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Victor Isaikin
+ */
+console.log('--------Number-------');
+let age = 40;
+console.log(age);
+console.log(-Infinity < 10000000000000000000000);
+console.log('Victor' / age);
+console.log(NaN * 100);
+
+console.log('--------BigInt-------');
+let bint = 100000000000000000000000000000000000n;
+console.log(bint);
+
+console.log('--------String-------');
+let userName = `Victor isaikin: ${age} years old`;
+console.log(userName);
+
+console.log('--------Boolen-------');
+let bool = 15 > 6;
+console.log(bool);
+
+console.log('--------Null-------');
+let n = null;
+console.log(n);
+
+console.log('--------Underfined-------');
+let u;
+console.log(u);
+
+console.log('--------typeof-------');
+console.log(typeof 10);
+console.log(typeof 10n);
+console.log(typeof 'Victor');
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Math);
