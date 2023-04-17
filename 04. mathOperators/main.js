4b08ae20a8d582765ce395c74033ba84bf5d1003
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-17 23:21:26 +0600

    04. mathOperators

---
+++ 04. mathOperators/main.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Victor Isaikin
+ */
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 / 2);
+console.log(2 * 2);
+
+console.log('------%------');
+console.log(5 % 2); // 5 - 2 = 3 - 2 = 1
+console.log(8 % 3); // 8 - 3 = 5 - 3 = 2
+console.log(8 % 4);
+
+console.log('------**------');
+// console.log(Math.pow(2, 2));
+console.log(2 ** 2);
+console.log(2 ** 3);
+console.log(2 ** 4);
+
+console.log('------+------');
+console.log('Victor' + ' Isaikin' + ': 40 years old');
+console.log('2' + 1);
+console.log(2 + 3 + '1');
+console.log('');
+let x = -1;
+// console.log(+x);
+console.log(+true);
+console.log(+'');
+
+console.log('------=------');
+let age = 40;
+console.log(age);
+
+let a, b, c;
+a = b = c = 2 + 3;
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log('-------------');
+
+let n = 2;
+// n = n + 4;
+n += 4;
+console.log(n);
+
+console.log('------++------');
+
+let counter = 2;
+// counter--;
+// --counter;
+// ++counter;
+counter++;
+console.log(counter);
+
+console.log('');
+
+let y = 3;
+let z = y--;
+console.log(y);
+console.log(z);
