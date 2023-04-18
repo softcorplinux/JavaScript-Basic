0bcdeeeb9ca9de889443e6872eb4b7d78b78d114
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-18 16:48:50 +0600

    05. comparisonOperators

---
+++ 05. comparisonOperators/main.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Victor Isaikin
+ */
+console.log(2 > 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 < 2);
+console.log('----------------');
+console.log(2 == 3);
+console.log(2 != 3);
+
+console.log('----------------');
+console.log('a' > 'b'); // 97 > 98 = false
+
+console.log('----------------');
+console.log('1' > 3);
+console.log('5' > 3);
+
+console.log('----------------');
+console.log(true == 1);
+console.log(false == 0);
+console.log(true == '1');
+
+console.log('----------------');
+console.log(true === 1);
+console.log(false === 0);
+
+console.log('----------------');
+console.log(null === undefined);
+console.log(null == undefined);
+
+console.log('----------------');
+console.log(null > 0); // 0 > 0 = false
+console.log(null == 0); // null == 0 = false
+console.log(null >= 0); // 0 >= 0 = true
+
+console.log('----------------');
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined == undefined);
