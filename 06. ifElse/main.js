fa680f744c9fde7a36989e554a13492d540cad2e
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-18 22:17:28 +0600

    06. ifElse

---
+++ 06. ifElse/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Victor Isaikin
+ */
+const sum = 2000;
+
+if (sum >= 10000 || sum == 3000) {
+  console.log('I will buy a book');
+} else if ((sum == 2000 && sum > 3000) || sum > 10000) {
+  console.log('I will buy a lot of books');
+} else {
+  console.log('I will not a buy a books');
+}
+
+sum == 2001 && sum < 3000
+  ? console.log('I have 5000')
+  : console.log('I dont have a money');
+
+const b = true;
+
+if (!b) {
+  // !!b = true или !b = false
+  console.log('I am here');
+}
