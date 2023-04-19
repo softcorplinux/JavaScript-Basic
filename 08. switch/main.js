ff13e5564bb3bd5b18ab4e54a581d815f722f230
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-19 20:33:54 +0600

    08. switch

---
+++ 08. switch/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Victor Isaikin
+ */
+let age = 30;
+
+switch (age) {
+  case 20:
+    console.log('You are 20 years old');
+    break;
+  case 30:
+    console.log('You are 30 years old');
+    break;
+  case 40:
+    console.log('You are 40 years old');
+    break;
+  case 50:
+    console.log('You are 50 years old');
+    break;
+  default:
+    console.log('Your age is unknow');
+    break;
+}
