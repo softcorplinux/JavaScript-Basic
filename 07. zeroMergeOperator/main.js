b54b0de68534a10dfe02482b54d2a2a09aff9429
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-19 19:49:23 +0600

    07. zeroMergeOperator

---
+++ 07. zeroMergeOperator/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Victor Isaikin
+ */
+let userName = '';
+
+console.log(userName ?? 'Victor');
+console.log(userName || 'Victor2');
+// console.log(userName !== null && userName !== undefined ? userName : 'Victor');
