f9f527375719fb0229609950d9d0099a9510f89c
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-19 08:46:37 +0600

    06. ifElse

--- 06. ifElse/main.js
+++ 06. ifElse/main.js
@@ -3,19 +3,19 @@
  */
 let sum = 2000;
 
-// if (sum >= 10000 || sum == 3000) {
-//   console.log('I will buy a book');
-// } else if (sum == 2001) {
-//   console.log('I will buy a lot of books');
-// } else if ((sum == 2000 && sum > 3000) || sum > 10000) {
-//   console.log('I will buy 100 books');
-// } else {
-//   console.log('I will not a buy a book');
-// }
+if (sum >= 10000 || sum == 3000) {
+  console.log('I will buy a book');
+} else if (sum == 2001) {
+  console.log('I will buy a lot of books');
+} else if ((sum == 2000 && sum > 3000) || sum > 10000) {
+  console.log('I will buy 100 books');
+} else {
+  console.log('I will not a buy a book');
+}
 
-// sum == 2001 || sum <= 3000
-//   ? console.log('I have 5000')
-//   : console.log('I dont have a money');
+sum == 2001 || sum <= 3000
+  ? console.log('I have 5000')
+  : console.log('I dont have a money');
 
 let b = false;
 console.log(!b);
