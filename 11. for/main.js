d6a2ba690f09e58103d546dde125772e6a0c3ade
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-21 11:00:57 +0600

    11. for

---
+++ 11. for/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Victor Isaikin
+ */
+
+for (let index = 0; index < 4; index++) {
+  console.log('Book ' + index);
+}
+
+// let index = 0;
+
+// for (; index < 4; ) {
+//   console.log('Book ' + index);
+//   index++;
+// }
+
+// let num = 100;
+
+// for (let index = 0, age = 40; index < 4; index++, age -= 10, num *= 20) {
+//   console.log('Index = ' + index + ' Age = ' + age + ' Num = ' + num);
+// }
